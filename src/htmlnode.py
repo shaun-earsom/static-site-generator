d7ad30bcabf517e748ad7debc08195a575317313
shaun-earsom <searsom@icloud.com>
2024-04-02 19:00:02 -0700

    Made the cursed recruision to_html for ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -32,5 +32,22 @@ class LeafNode(HTMLNode):
     
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        print(ParentNode)
+        if self.tag is None:
+            raise ValueError("Tag must be provided")
+        if self.children is None:
+            raise ValueError("Children must be provided")
+        child_arg = ""
+        for child in self.children:
+            child_arg += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{child_arg}</{self.tag}>"
     
-        
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,6 +2,7 @@ import unittest
 
 from htmlnode import HTMLNode
 from htmlnode import LeafNode
+from htmlnode import ParentNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -17,11 +18,24 @@ class TestTextNode(unittest.TestCase):
         
         self.assertEqual(result, "<p>This is a test.</p>")
 
-    def test_lefnodehref(self):
+    def test_leafnodehref(self):
         node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
         result = node.to_html()
 
         self.assertEqual(result, '<a href="https://www.google.com">Click me!</a>')
+    
+    def test_parentnode(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ]
+        )
+        result = node.to_html()
+        self.assertEqual(result, "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>")
 
 if __name__ == "__main__":
     unittest.main()
