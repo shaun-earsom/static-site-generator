debb19ecd497426ea46ca3a02cf811bd5d130f85
shaun-earsom <searsom@icloud.com>
2024-04-15 17:01:52 -0700

    mad because loop in loop

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -50,11 +50,47 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("cow", "image", "https://www.cows.com/cow.gif")
         node2 = text_node_to_html_node(node)
         self.assertEqual(node2, LeafNode("img", "",{"src": "https://www.cows.com/cow.gif", "alt": "cow"}))
-    
-    def test_splitNodeDelimBld(self):
-        node = TextNode("This is **bold** text.", "text")
+
+class TestInlineMarkdown(unittest.TestCase):
+    def test_delim_bold(self):
+        node = TextNode("This is text with a **bolded** word", "text")
         new_nodes = split_nodes_delimiter([node], "**", "bold")
-        self.assertEqual(new_nodes, [TextNode("This is ", "text"), TextNode("bold", "bold"), TextNode(" text.", "text")])
+        self.assertListEqual([TextNode("This is text with a ", "text"), TextNode("bolded", "bold"), TextNode(" word", "text")], new_nodes)
+
+    def test_delim_bold_double(self):
+        node = TextNode("This is text with a **bolded** word and **another**", "text")
+        new_nodes = split_nodes_delimiter([node], "**", "bold")
+        self.assertListEqual([TextNode("This is text with a ", "text"), TextNode("bolded", "bold"), TextNode(" word and ", "text"), TextNode("another", "bold")], new_nodes)
+
+    def test_delim_bold_multiword(self):
+        node = TextNode("This is text with a **bolded word** and **another**", "text")
+        new_nodes = split_nodes_delimiter([node], "**", "bold")
+        self.assertListEqual([TextNode("This is text with a ", "text"), TextNode("bolded word", "bold"), TextNode(" and ", "text"), TextNode("another", "bold")], new_nodes)
+
+    def test_delim_italic(self):
+        node = TextNode("This is text with an *italic* word", "text")
+        new_nodes = split_nodes_delimiter([node], "*", "italic")
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", "text"),
+                TextNode("italic", "italic"),
+                TextNode(" word", "text"),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_code(self):
+        node = TextNode("This is text with a `code block` word", "text")
+        new_nodes = split_nodes_delimiter([node], "`", "code")
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", "text"),
+                TextNode("code block", "code"),
+                TextNode(" word", "text"),
+            ],
+            new_nodes,
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -35,21 +35,22 @@ def text_node_to_html_node(text_node):
     raise Exception(f"Not supported text type in text_node_to_html_node: {text_node.text_type}.")
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    new_node_list = []
-    
-    for node in old_nodes:
-        if not isinstance(node, TextNode):
-            new_node_list.append(old_nodes)
-    this_node = node.text
-    node_list = this_node.split(delimiter)
-
-    for i in range(len(node_list)):
-        if i == 0:
-            new_node_list.append(TextNode(node_list[i], text_type_text))
-        elif i % 2 != 0:
-            new_node_list.append(TextNode(node_list[i], text_type))
-        else:
-            new_node_list.append(TextNode(node_list[i], text_type_text))
-    
-    return new_node_list
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != text_type_text:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], text_type_text))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
         
